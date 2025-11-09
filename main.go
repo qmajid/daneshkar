97b3296532c588f0f5654a4e5dac4fc02bc7e3bb
majid <majid.asgari.cs@gmail.com>
2025-11-09 18:33:18 +0330

    refactor

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
-	if err := r.Run(":8081"); err != nil {
+	if err := r.Run(); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
 }
