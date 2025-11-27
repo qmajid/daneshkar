25ef7788a1fd7a75cacf197455180d65fa710aae
majid <majid.asgari.cs@gmail.com>
2025-11-27 15:27:02 +0330

    gracefull shutdown

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	v1 "github.com/qmajid/daneshkar/internal/route/v1"
@@ -42,9 +47,34 @@ var serveCmd = &cobra.Command{
 
 		v1.InitRoute(r)
 
-		if err := r.Run(addr); err != nil {
-			log.Fatalf("failed to run server: %v", err)
+		srv := &http.Server{
+			Addr:    addr,
+			Handler: r.Handler(),
 		}
+
+		go func() {
+			// service connections
+			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Fatalf("listen: %s\n", err)
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		quit := make(chan os.Signal, 1)
+		// kill (no params) by default sends syscall.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Println("Gracefull Shutdown Server ...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Println("Server Shutdown:", err)
+		}
+		log.Println("Server exiting")
 	},
 }
 
